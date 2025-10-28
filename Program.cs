f77e71c5b1a1b35f75933a79b0d13011ee2a60d6
VAIII <danilkasuralev@gmail.com>
2025-10-28 11:14:30 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Practic11
-{
-    internal class Program
+Task 1 - CAR    
     {
         static void Main(string[] args)
         {
@@ -16,6 +8,77 @@ namespace Practic11
             myCar.Drive(); 
             myCar.Speed = -50;
             myCar.Drive();
+        }
+    }
+}
+Task 2 - Phone 
+        static void Main(string[] args)
+        {
+            Phone myPhone = new Phone("Samsung", 150); 
+            myPhone.Use(); 
+            myPhone.Use(); 
+            myPhone.Use(); 
+        }
+    }
+}
+Task 3 - Book
+        static void Main(string[] args)
+        {
+            try
+            {
+                Book myBook = new Book(""); 
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message); 
+            }
+            Book warAndPeace = new Book("Война и мир");
+            warAndPeace.Pages = 1200; 
+            warAndPeace.Info(); 
+            Book shortHistory = new Book("Краткая история");
+            shortHistory.Info(); 
+        }
+    }
+}
+Task 4 - Player
+        static void Main(string[] args)
+        {
+            try
+            {
+                Player player = new Player("Alex", 0, 100); 
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Player player2 = new Player("Alex", 5, 70);
+            player2.TakeDamage(20);
+            player2.TakeDamage(40); 
+            player2.TakeDamage(10);
+        }
+    }
+}
+Task 5 - Product
+    static void Main(string[] args)
+    {
+        try
+        {
+            Product laptop = new Product("Ноутбук", 1000, 20);
+            laptop.Show();
+            laptop.Discount = 10;
+            laptop.Show();
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        try
+        {
+            Product invalidProduct = new Product("Неправильный продукт", -100, 20);
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
         }
     }
 }
